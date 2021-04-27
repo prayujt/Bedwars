a37dc3b2cbe211b107378ec1b14c6bf838d939bf
cosmicbread <prayujtuli@hotmail.com>
2021-04-27 17:26:42 -0400

    Added logic for implementation of new objects

--- src/main/java/bedwars/PluginCore.java
+++ src/main/java/bedwars/PluginCore.java
@@ -7,21 +7,24 @@ import org.bukkit.entity.Player;
 import java.util.HashMap;
 
 import bedwars.commands.StartCommand;
+import bedwars.listeners.JoinEventListener;
+import bedwars.listeners.QuitEventListener;
 import bedwars.listeners.RespawnEventListener;
 
 public class PluginCore extends JavaPlugin {
 
     @Override
     public void onEnable(){
-        //When server plugin is first enabled
+        // Fired when server plugin is first enabled
         this.getCommand("start").setExecutor(new StartCommand());
         RespawnEventListener spawnListener = new RespawnEventListener(this);
-        
+        JoinEventListener joinListener = new JoinEventListener(this);
+        QuitEventListener quitListener = new QuitEventListener(this);
     }
 
     @Override
     public void onDisable(){
-        //Fired when the server stops and disables all plugins
+        // Fired when the server stops and disables all plugins
     }
 
 }
