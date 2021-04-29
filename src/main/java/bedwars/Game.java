d9f06a283fff002f087557f4d32c6ec053415838
cosmicbread <prayujtuli@hotmail.com>
2021-04-29 11:19:51 -0400

    Added objects for spawners

--- src/main/java/bedwars/Game.java
+++ src/main/java/bedwars/Game.java
@@ -6,8 +6,12 @@ import java.util.*;
 
 public class Game {
     public static ArrayList<String> colors = new ArrayList<String>(List.of("Yellow", "Red", "White"));
-    public static Hashtable<Integer, ArrayList<BedwarsPlayer>> teams = new Hashtable<Integer, ArrayList<BedwarsPlayer>>();
     public static ArrayList<Player> onlinePlayers = new ArrayList<Player>();
+
+    public static Hashtable<Integer, ArrayList<BedwarsPlayer>> teams = new Hashtable<Integer, ArrayList<BedwarsPlayer>>();
+    public static Hashtable<Integer, ArrayList<Integer>> baseSpawners = new Hashtable<Integer, ArrayList<Integer>>();
+    public static Hashtable<Integer, ArrayList<Integer>> diamondSpawners = new Hashtable<Integer, ArrayList<Integer>>();
+    public static Hashtable<Integer, ArrayList<Integer>> emeraldSpawners = new Hashtable<Integer, ArrayList<Integer>>();
 
     public static void addPlayer(String name, int team, int x, int y, int z) {
         BedwarsPlayer bp = new BedwarsPlayer(name, team, x, y, z);
