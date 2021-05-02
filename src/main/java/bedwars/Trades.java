732e579711bc080b4efb4ff5b8fd15cf7030b6c0
cosmicbread <prayujtuli@hotmail.com>
2021-05-02 16:07:21 -0400

    Removed test trades

--- src/main/java/bedwars/Trades.java
+++ src/main/java/bedwars/Trades.java
@@ -9,7 +9,6 @@ public class Trades {
     // Trades are added in the form: new Trade(Trade_Name, Material for player to recieve, amount to recieve, Material for player to give, amount to give)
     public static ArrayList<Trade> trades = new ArrayList<Trade>(List.of(
                 new Trade("Fireball", Material.FIRE_CHARGE, 1, Material.IRON_INGOT, 4),
-                new Trade("White Wool", Material.WHITE_WOOL, 1, Material.IRON_INGOT, 1)
     ));
 
     public static Trade getTrade(Material material) {
