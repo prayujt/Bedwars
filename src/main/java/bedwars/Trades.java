5fa1ea072c427e919938a1c7eb12f7beb2684f2a
cosmicbread <prayujtuli@hotmail.com>
2021-05-03 13:02:00 -0400

    Added more trades to list

--- src/main/java/bedwars/Trades.java
+++ src/main/java/bedwars/Trades.java
@@ -19,10 +19,15 @@ public class Trades {
                 new Trade("Iron Leggings", Material.IRON_LEGGINGS, 1, Material.IRON_INGOT, 15, blank),
                 new Trade("Iron Helmet", Material.IRON_HELMET, 1, Material.IRON_INGOT, 10, blank),
                 new Trade("Iron Boots", Material.IRON_BOOTS, 1, Material.IRON_INGOT, 10, blank),
-                new Trade("Golden Apple", Material.IRON_HELMET, 1, Material.GOLD_INGOT, 10, blank),
-                new Trade("Bow", Material.BOW, 1, Material.GOLD_INGOT, 12, blank),
+                new Trade("Golden Apple", Material.GOLDEN_APPLE, 1, Material.GOLD_INGOT, 10, blank),
+                new Trade("Enchanted Golden Apple", Material.ENCHANTED_GOLDEN_APPLE, 1, Material.GOLD_INGOT, 50, blank),
+                new Trade("Bow", Material.BOW, 1, Material.GOLD_INGOT, 12, new HashMap<Enchantment, Integer>(Map.of(Enchantment.VANISHING_CURSE, 1))),
                 new Trade("Arrow", Material.SPECTRAL_ARROW, 8, Material.GOLD_INGOT, 2, blank),
-                new Trade("Emerald Bow", Material.BOW, 1, Material.GOLD_INGOT, 2, new HashMap<Enchantment, Integer>(Map.of(Enchantment.ARROW_KNOCKBACK, 2, Enchantment.DURABILITY, 2, Enchantment.ARROW_DAMAGE, 2)))
+                new Trade("Emerald Bow", Material.BOW, 1, Material.EMERALD, 6, new HashMap<Enchantment, Integer>(Map.of(Enchantment.ARROW_KNOCKBACK, 2, Enchantment.DURABILITY, 2, Enchantment.ARROW_DAMAGE, 2, Enchantment.VANISHING_CURSE, 1))),
+                new Trade("Endstone", Material.END_STONE, 12, Material.IRON_INGOT, 24, blank),
+                new Trade("Wood", Material.OAK_PLANKS, 8, Material.GOLD_INGOT, 12, blank),
+                new Trade("Pickaxe", Material.IRON_PICKAXE, 1, Material.GOLD_INGOT, 10, blank),
+                new Trade("Axe", Material.IRON_AXE, 1, Material.GOLD_INGOT, 10, blank)
     ));
 
     public static Trade getTrade(String displayName) {
