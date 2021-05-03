55881549b248a4816203f894ff1ab7a14e0081d3
cosmicbread <prayujtuli@hotmail.com>
2021-05-02 22:49:39 -0400

    Added back conditions for joining when not in-game

--- src/main/java/bedwars/listeners/JoinEventListener.java
+++ src/main/java/bedwars/listeners/JoinEventListener.java
@@ -6,6 +6,7 @@ import bedwars.Game;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -17,6 +18,7 @@ public class JoinEventListener implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         System.out.println("Player joined! " + event.getPlayer().getName());
         Game.onlinePlayers.add(event.getPlayer());
+        if (!Game.getPlayer(event.getPlayer()).inGame) event.getPlayer().teleport(new Location(Game.getWorld(), 0, 118, 0));
         System.out.println(Game.onlinePlayers.toString());
     }
 }
